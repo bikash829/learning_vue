5c00717175f994feaf61aa5cf3d766ff6f1a18d6
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-02-22 14:04:55 +0600

    nav bar designed

--- day1/src/App.vue
+++ day1/src/App.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <nav class="nav-container">
+  <!-- <nav class="nav-container">
     <div class="nav-wrapper">
       <div class="nav__logo">
         <h2>Nav Logo</h2>
-        <!-- <img src="#" alt="Nav Logo"> -->
+        <img src="#" alt="Nav Logo">
       </div>
       <div class="nav__menu">
           <ul class="nav__menu">
@@ -27,7 +27,28 @@
       </div>
 
     </div>
+  </nav> -->
+  <header class="navbar">
+  <div class="navbar__brand">
+    <!-- <a class="link_normal" href="#"><img src="#" alt="Brand Logo" class="navbar__logo"></a> -->
+    <a class="link_normal" href="#"><h3>Brand <span>Logo</span></h3></a>
+    
+  </div>
+  <nav class="nav">
+    <ul class="nav__list">
+      <li class="nav__item"><a href="#" class="nav__link">Home</a></li>
+      <li class="nav__item"><a href="#" class="nav__link">About</a></li>
+      <li class="nav__item"><a href="#" class="nav__link">Services</a></li>
+      <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+    </ul>
   </nav>
+  <div class="social">
+    <a href="#" class="social__link"><i class="fa-brands fa-facebook-f"></i></a>
+    <a href="#" class="social__link"><i class="fa-brands fa-x-twitter"></i></a>
+    <a href="#" class="social__link"><i class="fa-brands fa-instagram"></i></a>
+  </div>
+</header>
+
 </template>
 
 <style scoped>
