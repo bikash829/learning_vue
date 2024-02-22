a0cdba8e3005dd13786ce01996eb78ef416d2f29
Bikash Chowdhury <chowdhurybikash38@gmail.com>
2024-02-22 12:17:20 +0600

    file configured

--- day1/src/App.vue
+++ day1/src/App.vue
@@ -3,7 +3,31 @@
 </script>
 
 <template>
-  <h1>Hello world</h1>
+  <nav class="nav-container">
+    <div class="nav-wrapper">
+      <div class="nav__logo">
+        <h2>Nav Logo</h2>
+        <!-- <img src="#" alt="Nav Logo"> -->
+      </div>
+      <div class="nav__menu">
+          <ul class="nav__menu">
+            <li class="nav__item"><a href="#" class="nav__link">Home</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">About</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Contact</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Services</a></li>
+            <li class="nav__item"><a href="#" class="nav__link">Profile</a></li>
+          </ul>
+      </div>
+      <div class="nav__social-icon">
+        <ul class="social-icon">
+          <li class="social-icon__item"><a href="#" class="social-icon__link"><i class="fa-brands fa-facebook-f"></i></a></li>
+          <li class="social-icon__item"><a href="#" class="social-icon__link"><i class="fa-brands fa-x-twitter"></i></a></li>
+          <li class="social-icon__item"><a href="#" class="social-icon__link"><i class="fa-brands fa-instagram"></i></a></li>
+        </ul>
+      </div>
+
+    </div>
+  </nav>
 </template>
 
 <style scoped>
